f43793426f905709f4feed5ba0ced1452342f8cb
arees2020 <andrew.rees2020@gmail.com>
2020-04-04 15:53:25 +0100

    feature/MockUnitTests-02 - Added additional mock tests

--- MockUnitTests.Tests/SampleCalcFromInterfaceTests.cs
+++ MockUnitTests.Tests/SampleCalcFromInterfaceTests.cs
@@ -79,5 +79,22 @@ namespace MockUnitTests.Tests
             //Assert
             Assert.IsTrue(multval.Equals(8));
         }
+
+        [Test]
+        public void Mock_DoCalc_Verify_Exception_Thrown_Calc()
+        {
+            //Arrange
+            var sampleClass = new Mock<ISampleClass>();
+            sampleClass.Setup(x => x.DoMultiply(It.IsAny<Int32>(), It.IsAny<Int32>()))
+                       .Returns((Int32 a, Int32 b) => a * b);
+
+            ISampleCalc sampleCalc = new SampleCalc(sampleClass.Object);
+
+            //Act
+            int toPower = -1;
+
+            //Assert
+            Assert.Throws<ArgumentException>(()=> sampleCalc.CalcToPower(2, toPower));
+        }
     }
 }
